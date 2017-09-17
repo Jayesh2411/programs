37422174548d9557d96755abed089e66c29cb74a
Jayesh Sinha <jsinha2411@gmail.com>
2017-09-17 07:53:40 +0530

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
-
 void itoa(long int num,char *string)	
 {
-	
-	
 	int i=0;
 	while(num > 0)	{
 		string[i]=num%10+'0';
 		i++;
 		num=num/10;
 	}
-	
-	
 	int len=i-1;
-	
 	char *buff;
 	buff = (char *)malloc(sizeof(char)*1000);
 	i=0;
@@ -25,21 +18,14 @@ void itoa(long int num,char *string)
 		i++;
 		len--;
 	}
-	
 	strcpy(string,buff);
-
 }
-
-
-int main()	{
-	
+int main()	{	
 	long int number;
 	printf("\n Enter the number: ");
-
 	scanf("%li",&number);
 	char *string;
 	string = (char *)malloc(sizeof(char)*1000);
 	itoa(number,string);
 	printf("\nThe string is :%s \n",string);
-
 }
